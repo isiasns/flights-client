f1f9b20fc616e228ff2d0db1dee7c545a8551861
Isaac Sias <isias@nearsoft.com>
2018-11-01 14:55:25 -0700

    Disabled inputs based on from and departing

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,13 +4,13 @@ import { and, notEmpty } from '@ember/object/computed'
 export default Controller.extend({
   from: "",
   to: "",
-  departingDate: "",
+  departing: "",
   returningDate: "",
   isValidFrom: notEmpty("from"),
   isValidTo: notEmpty("to"),
   isValidDeparting: notEmpty("departing"),
   isValidReturning: notEmpty("returning"),
-  isValid: and("isValidFrom", "isValidTo", "isValidDeparting", "isValidReturning"),
+  isValidForm: and("isValidFrom", "isValidTo", "isValidDeparting", "isValidReturning"),
   actions: {
     searchFlights() {
       console.log(this.isValidFrom, this.isValidTo, this.isValidDeparting, this.isValidReturning)
