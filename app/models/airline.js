819f93d1665c8e0f1e3f8270cd5fe3b0e0da31f3
Isaac Sias <isias@nearsoft.com>
2018-11-02 10:09:42 -0700

    Removed innecesary attributes

--- app/models/airline.js
+++ app/models/airline.js
@@ -2,8 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   fs: DS.attr(),
-  iata: DS.attr(),
-  icao: DS.attr(),
-  name: DS.attr(),
-  active: DS.attr()
+  name: DS.attr()
 });
