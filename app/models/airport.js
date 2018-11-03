24c5912cd220bfbfc49da0a9c8e51926d05e6f9c
Isaac Sias <isias@nearsoft.com>
2018-11-03 16:06:48 -0700

    Fixed missing semicolon and added attribute to get full airport name

--- app/models/airport.js
+++ app/models/airport.js
@@ -1,7 +1,11 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object'
 
 export default DS.Model.extend({
   name: DS.attr("string"),
   city: DS.attr("string"),
-  countryName: DS.attr("string")
+  countryName: DS.attr("string"),
+  full: computed("id", "name", "city", () => {
+    return `${this.city}, ${this.name} (${this.id})`;
+  })
 });
